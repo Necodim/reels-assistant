93c5ecb459eb18af1b82ed2a97cc63c42e927e56
solowey <solowey1@gmail.com>
2024-04-11 21:22:38 +0300

    upd link

--- src/bot/helpers/products.js
+++ src/bot/helpers/products.js
@@ -1,6 +1,6 @@
 const link = (name, amount) => {
   // http://api.reelsassistant.ru/cloudpayments?
-  return `https://api.reelsassistant.ru/cloudpayments&amount=${encodeURIComponent(amount)}&name=${encodeURIComponent(name)}`;
+  return `https://api.reelsassistant.ru/cloudpayments?amount=${encodeURIComponent(amount)}&name=${encodeURIComponent(name)}`;
 }
 
 const products = [
