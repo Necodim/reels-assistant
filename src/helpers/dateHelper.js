d5d0f79d4a4af0bf490c6dd491249046a91e33c7
solowey <solowey1@gmail.com>
2024-04-12 17:09:58 +0300

    fix dateFormat

--- src/helpers/dateHelper.js
+++ src/helpers/dateHelper.js
@@ -3,8 +3,8 @@ const { ru } = require('date-fns/locale');
 
 // Формат 'd MMMM, HH:mm' выведет '12 июня, 15:47'
 
-const formatDate = (date, format) => {
-  const formatedDate = format(new Date(date), format, { locale: ru });
+const formatDate = (date, dateFormat) => {
+  const formatedDate = format(new Date(date), dateFormat, { locale: ru });
   return formatedDate;
 };
 
