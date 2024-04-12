edd8904bcd07fc91123fb7984cd2e254a047110e
solowey <solowey1@gmail.com>
2024-04-12 14:45:46 +0300

    calculator

--- src/payments/hmacCalculator.js
+++ src/payments/hmacCalculator.js
@@ -2,15 +2,18 @@ require('dotenv').config();
 const crypto = require('crypto');
 
 function calculateHMAC(req) {
-    let payload;
-    if (req.headers['content-type'] === 'application/json') {
-      payload = JSON.stringify(req.body);
-    } else {
-      payload = req.body;
-    }
-    const hmac = crypto.createHmac('sha256', process.env.CLOUDPAYMENTS_API_KEY);
-    hmac.update(payload, 'utf8');
-    return hmac.digest('base64');
+  console.log('headers', req.headers)
+  console.log('body', typeof req.body, req.body)
+  let payload;
+  if (req.headers['content-type'] === 'application/json') {
+    payload = JSON.stringify(req.body);
+  } else {
+    payload = req.body;
+  }
+  console.log('payload', typeof payload, payload)
+  const hmac = crypto.createHmac('sha256', process.env.CLOUDPAYMENTS_API_KEY);
+  hmac.update(payload, 'utf8');
+  return hmac.digest('base64');
 }
 
 module.exports = calculateHMAC;
