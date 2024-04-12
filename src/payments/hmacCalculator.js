8beacf323361062a89079d7fb39630f5d09c1770
solowey <solowey1@gmail.com>
2024-04-12 15:08:26 +0300

    const payload = new URLSearchParams(req.body).toString();

--- src/payments/hmacCalculator.js
+++ src/payments/hmacCalculator.js
@@ -6,7 +6,7 @@ function calculateHMAC(req) {
   console.log('body', typeof req.body, req.body)
   let payload;
   if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
-    payload = JSON.stringify(req.body);
+    payload = new URLSearchParams(req.body).toString();
   } else if (req.headers['content-type'] === 'application/json') {
     payload = JSON.stringify(req.body);
   } else {
